909874026f79d654f573f54032b9c4bde0840566
Illia Zviezdin <82528000+KetoAcc2@users.noreply.github.com>
2021-09-18 18:15:22 +0200

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlazorServerApp.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Studies { get; set; }
+    }
+}
